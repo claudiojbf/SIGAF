1bb61738db4d8bb03f2a0ff2c13c8e0874af9c00
Claudio Jose Bezerra Filho <65622389+claudiojbf@users.noreply.github.com>
2022-11-27 17:10:51 -0300

    Adcionado algumas paginas da consulta

--- App/Consultas/urls.py
+++ App/Consultas/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('index_consultas/', views.index_consultas, name="index_consultas"),
-    path('visualizar_atleta/<int:atleta_id>', views.visualizar_atleta_consulta, name="visualizar_atleta_consulta")
+    path('visualizar_atleta/<int:atleta_id>', views.visualizar_atleta_consulta, name="visualizar_atleta_consulta"),
+    path('listar_consultas/<int:atleta_id>', views.listar_consultas, name="listar_consultas"),
+    path('criar_consulta/<int:atleta_id>', views.criar_consulta, name="criar_consulta"),
+    path('consulta_geral/<int:consulta_id>',views.consulta_geral, name="consulta_geral" )
 ]
--- App/Consultas/views.py
+++ App/Consultas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import *
 from App.Usuario.models import Usuario
 from App.Atletas.models import Atleta
 
@@ -33,3 +34,41 @@ def visualizar_atleta_consulta(request, atleta_id):
     }
 
     return render(request, "consultas/visualizar_atleta_consulta.html", dados)
+
+@login_required(login_url='login')
+def listar_consultas(request, atleta_id):
+    usuario = request.user.id
+    usuario_i = Usuario.objects.get(usuario_id = usuario)
+    atleta = get_object_or_404(Atleta, pk = atleta_id)
+    consultas = Consulta.objects.filter(atleta_id = atleta)
+    
+    dados = {
+        "usuario":usuario_i,
+        "consultas":consultas,
+        "atleta" :atleta_id,
+    }
+
+    return render(request, 'consultas/listar_consultas.html', dados)
+
+@login_required(login_url='login')
+def criar_consulta(request, atleta_id):
+    atleta = get_object_or_404(Atleta, pk = atleta_id)
+    consulta = Consulta.objects.create(atleta = atleta)
+    consulta.save()
+
+    return redirect("listar_consultas", atleta_id)
+
+@login_required(login_url='login')
+def consulta_geral(request, consulta_id):
+    usuario = request.user.id
+    usuario_i = Usuario.objects.get(usuario_id = usuario)
+    consultas = get_object_or_404(Consulta, pk = consulta_id)
+    entrada = Entrada.objects.get(consulta_id = consultas).exists()
+    print(entrada)
+
+    dados = {
+        "usuario":usuario_i,
+        "consulta":consultas,
+    }
+
+    return render(request, 'consultas/consulta_atleta.html', dados)
